f9c7eb96923306e24887b77b09c55502e28920d9
Haidwang <smithw1989@icloud.com>
2016-04-10 23:02:43 +0800

    add more

--- src/com/nowcoder/PrintListFromTailToHead.java
+++ src/com/nowcoder/PrintListFromTailToHead.java
@@ -1,7 +1,45 @@
 package com.nowcoder;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 /**
  * Created by Bauhaus on 2016/4/6.
  */
 public class PrintListFromTailToHead {
+
+	public static void main(String[] args) {
+
+		ListNode listNode = new ListNode(2);
+
+
+		ArrayList<Integer> a = printListFromTailToHead(listNode);
+		for (int i = 0; i < a.size(); i++) {
+			System.out.println(a.get(i));
+		}
+
+	}
+
+	public static ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+		Stack<Integer> stack = new Stack<>();
+		while (listNode != null) {
+			stack.push(listNode.val);
+			listNode = listNode.next;
+		}
+		ArrayList<Integer> list = new ArrayList<>();
+		while (!stack.isEmpty()) {
+			list.add(stack.pop());
+		}
+		return list;
+	}
+
+	public static class ListNode {
+		int val;
+
+		ListNode next = null;
+
+		ListNode(int val) {
+			this.val = val;
+		}
+	}
 }
---
+++ src/com/nowcoder/ReConstructBinaryTree.java
@@ -0,0 +1,74 @@
+package com.nowcoder;
+
+import java.util.HashMap;
+
+/**
+ * Created by Bauhaus on 2016/4/10.
+ */
+public class ReConstructBinaryTree {
+	public static void main(String[] args) {
+
+		int[] pre = {1, 2, 4, 7, 3, 5, 6, 8};
+		int[] in = {4, 7, 2, 1, 5, 3, 8, 6};
+		TreeNode node = reConstructBinaryTree(pre, in);
+
+	}
+
+	private static TreeNode root;
+
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	public static TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+		if (pre == null || in == null) {
+			return null;
+		}
+
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < in.length; i++) {
+			map.put(in[i], i);
+		}
+		return preIn(pre, 0, pre.length - 1, in, 0, in.length - 1, map);
+
+	}
+
+	private static TreeNode preIn(int[] pre, int startPre, int endPre, int[] in, int startIn, int endIn, HashMap<Integer, Integer> map) {
+		if (startPre > endPre) {
+			return null;
+		}
+
+		TreeNode head = new TreeNode(pre[startPre]);
+		int index = map.get(pre[startPre]);
+		head.left = preIn(pre, startPre + 1, startPre + index - startIn, in, startIn, index - 1, map);
+		head.right = preIn(pre, startPre + index - startIn + 1, endPre, in, index + 1, endIn, map);
+		return head;
+	}
+
+	public static void buildTree(TreeNode node, int data) {
+		if (root == null) {
+			root = new TreeNode(data);
+		} else {
+			if (data < node.val) {
+				if (node.left == null) {
+					node.left = new TreeNode(data);
+				} else {
+					buildTree(node.left, data);
+				}
+			} else {
+				if (node.right == null) {
+					node.right = new TreeNode(data);
+				} else {
+					buildTree(node.right, data);
+				}
+			}
+
+		}
+	}
+}
